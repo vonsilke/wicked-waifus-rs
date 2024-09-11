9afba329c2b4d30b95cba52e5a0f4114f1cd4292
a <a>
2024-09-11 20:26:09 +0200

    Initial docker test
    Signed-off-by: xavo95 <xavo95@xeondev.com>

---
+++ builder.bat
@@ -0,0 +1,9 @@
+docker build -t shorekeeper-builder:1.3.0-SNAPSHOT -f Dockerfile-builder .
+
+docker build -t shorekeeper-config-server:1.3.0-SNAPSHOT --build-arg MICROSERVICE=config-server -f Dockerfile-service .
+docker build -t shorekeeper-hotpatch-server:1.3.0-SNAPSHOT --build-arg MICROSERVICE=hotpatch-server -f Dockerfile-service .
+docker build -t shorekeeper-login-server:1.3.0-SNAPSHOT --build-arg MICROSERVICE=login-server -f Dockerfile-service .
+docker build -t shorekeeper-gateway-server:1.3.0-SNAPSHOT --build-arg MICROSERVICE=gateway-server -f Dockerfile-service .
+docker build -t shorekeeper-game-server:1.3.0-SNAPSHOT --build-arg MICROSERVICE=game-server -f Dockerfile-service .
+
+docker rmi shorekeeper-builder:1.3.0-SNAPSHOT
